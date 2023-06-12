565b533610b8362eddcf7c280ffc29fdc918e9fa
borilio <exprimetuandroid@gmail.com>
2023-06-12 12:19:02 +0200

    Puesta a punto para distribución final

---
+++ src/app/components/jabonera/jabonera.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jabonera',
+  templateUrl: './jabonera.component.html',
+  styleUrls: ['./jabonera.component.css']
+})
+export class JaboneraComponent {
+
+}
